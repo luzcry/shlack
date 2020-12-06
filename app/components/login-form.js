19f3fd2456a8819aba5a29e53827719e1cc60607
luzcry <mcrodriguezt618@gmail.com>
2020-12-06 02:32:02 -0500

    service auth

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,17 +1,20 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import AuthService from 'shlack/services/auth';
 
 export default class LoginFormComponent extends Component {
   @tracked
   userId = null;
+  /**
+   * @type {AuthService}
+   */
+
+  @service auth;
 
   get isDisabled() {
     return !this.userId;
-  }
-
-  loginAsUserWithId(val){
-    console.log('UserId: ', val);
   }
 
 /**
@@ -32,6 +35,6 @@ onLoginFormSubmit(evt){
   const { target } = evt;
   const val = target.querySelector('select').value;
   evt.preventDefault();
-  this.loginAsUserWithId(val);
+  this.auth.loginWithUserId(val);
   }
 }
