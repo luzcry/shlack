25ccf35d92af09a4868d747d553ac71e801b4158
luzcry <mcrodriguezt618@gmail.com>
2020-12-06 02:16:26 -0500

    button login form integration test

--- tests/integration/components/login-form-test.js
+++ tests/integration/components/login-form-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, fillIn, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | login-form', function(hooks) {
@@ -21,9 +21,13 @@ module('Integration | Component | login-form', function(hooks) {
           "login",
           "select a user",
           "testy Testerson",
-          "Sample McData",
-          "A validation message"
+          "Sample McData"
         ]
       );
-    })
+      let button = /** @type {HTMLInputElement}*/ (find('input[type="submit"]'));
+      assert.equal(button.disabled, true);
+      //button is disabled
+      await fillIn('select', '1');
+      assert.equal(button.disabled, false);
+    });
   });
