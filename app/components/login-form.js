17089cbf6055d91c0239000e75d0237403e07b6f
luzcry <mcrodriguezt618@gmail.com>
2020-12-06 01:58:54 -0500

    tracked properties usage

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,15 +1,29 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class LoginFormComponent extends Component {
-
+  @tracked
   userId = null;
-  
+
+  get isDisabled() {
+    return !this.userId;
+  }
+
   loginAsUserWithId(val){
     console.log('UserId: ', val);
   }
 
-  /**
+/**
+ *
+ *  @param {Event & { target: HTMLSelectElement }} evt
+ * */
+
+@action
+onSelectChanged(evt){
+  this.userId = evt.target.value;
+}
+ /**
    *
    * @param {Event & {target: HTMLFormElement}} evt
 */
