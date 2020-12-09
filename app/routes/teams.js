583faf8114d854dc72e824b2209ad3c820628d6e
luzcry <mcrodriguezt618@gmail.com>
2020-12-08 23:31:49 -0500

    url state in UI

--- app/routes/teams.js
+++ app/routes/teams.js
@@ -2,6 +2,32 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import AuthService from 'shlack/services/auth';
 
+const ALL_TEAMS = [
+  {
+    "id": "linkedin",
+    "name": "LinkedIn",
+    "order": 2,
+    "iconUrl": "/assets/img/linkedin.png"
+  },
+  {
+    "id": "ms",
+    "name": "Microsoft",
+    "order": 3,
+    "iconUrl": "/assets/img/microsoft.png"
+  },
+  {
+    "id": "avengers",
+    "name": "Avengers",
+    "order": 4,
+    "iconUrl": "/assets/img/avengers.jpg"
+  },
+  {
+    "id": "angrycat",
+    "name": "Angry Cat",
+    "order": 5,
+    "iconUrl": "/assets/img/angry-cat.jpg"
+  }
+]
 export default class TeamsRoute extends Route {
   /**
    *
@@ -14,5 +40,8 @@ export default class TeamsRoute extends Route {
     if (this.auth.currentUserId) {
       this.transitionTo('login');
     }
+  }
+  async model() {
+    return ALL_TEAMS;
   }
 }
